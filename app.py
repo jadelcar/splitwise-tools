fe8e95767d81b6021ce0054632eb1f976de88433
Javier Delgado Carbajo <jadelcar@gmail.com>
2022-10-20 18:26:23 +0200

    Early version of splitwise-tools

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, url_for, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_session.__init__ import Session
 from datetime import datetime
 
 
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 #Initalize the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # three '/' is relative path, 4 would be aboslute
