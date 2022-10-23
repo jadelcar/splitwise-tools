fc631b84bc5cf5874fc829e59979828a141a3958
Javier Delgado Carbajo <jadelcar@gmail.com>
2022-10-23 20:37:08 +0200

    None

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, url_for, render_template, request, redirect
+from flask import Flask, url_for, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_session.__init__ import Session
 from datetime import datetime
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import apology, login_required, lookup, usd, update_portfolios
 
 app = Flask(__name__)
 
@@ -12,6 +15,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
 Session(app)
 
 #Initalize the database
@@ -45,6 +49,29 @@ class Todo(db.Model):
     #Function that returns a string every time we create a new element
     def __repr__(self):
         return '<Task %r>' % self.id
+
+class User(db.Model):
+    #__tablename__ = "to_dos" # Define the table name (Not in the tutorial)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False) #nullable false means the user cannot leave it null (empty)
+    #completed =db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    pass_hash = db.Column(db.String(200), nullable=False) 
+    split_user_id = db.Column(db.String(40))
+    
+    #Function that returns the ID every time we create a new element
+    def __repr__(self):
+        return '<You were assigned ID %r>' % self.id
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route('/', methods=['GET','POST'])
@@ -97,6 +124,91 @@ def update(id):
 
     else:
         return render_template('update.html', task=task)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = User.query.filter_by(username=request.form.get("username")).__dict__
+        # rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["pass_hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    #If they reach via "GET" we redirect to the page for registering
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # O/w they reach via post, meaning that they have entered name and password
+    else:
+        # Obtain user's data entered in the form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Validate data
+        if username=="" or password=="" or confirmation=="" :
+            # If at least one of the above are missing
+            return apology("Sorry, username or password was missing...", 400)
+
+        elif password != confirmation :
+            return apology("Sorry, passwords don't match")
+
+        # Hash the password
+        password_hashed = generate_password_hash(password)
+
+        new_user = User(username=username, pass_hash=password_hashed)
+        # Insert into table "users", initializing cash to a starting value (initial_cash)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except:
+            return apology("Could not insert you in the database")
+        # Redirect to index (showing a summary of stocks owned and cash)
+    return render_template("base.html")
+
+
 
 
 if __name__ == "__main__":
